a40994207215ee56e4b8886e44a99ba08cb3b475
Otavio Dias <otaviodias@gmail.com>
2026-02-11 21:49:16 +0000

    change base to /pintar-sem-medo

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/lp-pintar-sem-medo/",
+  base: "/pintar-sem-medo/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
